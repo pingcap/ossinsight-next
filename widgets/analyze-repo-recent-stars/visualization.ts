3e4941701330a407ae7a8a9287501d7f7639f17b
czhen <56986964+shczhen@users.noreply.github.com>
2023-07-20 17:37:50 +0800

    widget: add @ossinsight/widget-analyze-repo-recent-stars

---
+++ widgets/analyze-repo-recent-stars/visualization.ts
@@ -0,0 +1,57 @@
+import type {
+  EChartsVisualizationConfig,
+  WidgetVisualizerContext,
+} from '@ossinsight/widgets-types';
+
+type Params = {
+  repo_id: string;
+};
+
+type DataPoint = {
+  current_period_day: string;
+  current_period_day_stars: number;
+  current_period_stars: number;
+  idx: number;
+  last_period_day: string;
+  last_period_day_stars: number;
+  last_period_stars: number;
+};
+
+type Input = [DataPoint[], DataPoint[] | undefined];
+
+export default function (
+  data: Input,
+  ctx: WidgetVisualizerContext<Params>
+): EChartsVisualizationConfig {
+  const [main, vs] = data;
+
+  return {
+    dataset: {
+      source: [...main.sort((a, b) => a.idx - b.idx)],
+    },
+    xAxis: {
+      type: 'time',
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: {
+      type: 'bar',
+      encode: {
+        x: 'current_period_day',
+        y: 'current_period_day_stars',
+      },
+      color: ctx.theme.colors.red['400'],
+      name: 'Stars',
+    },
+    tooltip: {
+      show: true,
+      trigger: 'axis',
+      axisPointer: {
+        type: 'line',
+      },
+    },
+  };
+}
+
+export const type = 'echarts';
