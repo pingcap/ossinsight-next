9c248824b28b0ba8ef795d8e8dff92b87448adc8
Jagger <634750802@qq.com>
2023-07-25 15:53:25 +0800

    fix

--- packages/widgets-core/src/parameters/react/index.tsx
+++ packages/widgets-core/src/parameters/react/index.tsx
@@ -1,8 +1,10 @@
 import { RepoIdInput } from './repo-id';
+import { UserIdInput } from './user-id';
 import { ParameterDefinition } from '@ossinsight/widgets-types'
 
 const types = {
   'repo-id': RepoIdInput,
+  'user-id': UserIdInput,
 };
 
 export function ParamInput ({ config, value, onValueChange }: { config: ParameterDefinition, value: any, onValueChange: (value: any) => void }) {
---
+++ packages/widgets-core/src/parameters/react/user-id/index.tsx
@@ -0,0 +1,28 @@
+import { GHRepoSelector, RemoteRepoInfo, RemoteSelectorInputProps } from '@ossinsight/ui';
+import { GHUserSelector, RemoteUserInfo } from '@ossinsight/ui/src/components/GHUserSelector';
+import { useCallback, useContext } from 'react';
+import { ParametersContext } from '../context';
+
+export function UserIdInput ({ value, onValueChange }: { value: number, onValueChange: (newValue: number) => void }) {
+  const { linkedData } = useContext(ParametersContext);
+
+  const handleUserSelected = useCallback((user: RemoteUserInfo) => {
+    linkedData.users[String(user.id)] = user;
+    onValueChange(user.id);
+  }, []);
+
+  const user = linkedData.users[String(value)];
+
+  return (
+    <GHUserSelector
+      user={user}
+      onUserSelected={handleUserSelected}
+      renderInput={renderInput}
+    />
+  );
+}
+
+function renderInput (props: RemoteSelectorInputProps) {
+  return <input className="TextInput" {...props}
+                type={(props as any).type === 'button' ? undefined : (props as any).type} />;
+}
