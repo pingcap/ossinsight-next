fae8ec7c4b8eae10feeb7bda2bc5b623ba0c235b
Jagger <634750802@qq.com>
2023-07-31 18:34:38 +0800

    support auto size

--- web/components/Share/Share.tsx
+++ web/components/Share/Share.tsx
@@ -2,8 +2,8 @@ import siteConfig from '@/site.config';
 import { widgetMetadataGenerator } from '@/utils/widgets';
 import { ShareBlock } from '@ossinsight/ui/src/components/ShareBlock';
 import { LinkedData } from '@ossinsight/widgets-core/src/parameters/resolver';
+import { generateZoneOptions, PERIOD_OPTIONS } from '@ossinsight/widgets-utils/src/ui';
 import * as colors from 'tailwindcss/colors';
-import { PERIOD_OPTIONS, generateZoneOptions } from '@ossinsight/widgets-utils/src/ui';
 
 export async function Share ({ name, params, searchParams, linkedDataPromise }: { name: string, params: any, searchParams: any, linkedDataPromise: Promise<LinkedData> }) {
   const generateMetadata = await widgetMetadataGenerator(name);
@@ -27,23 +27,25 @@ export async function Share ({ name, params, searchParams, linkedDataPromise }: 
     getOrg (id: number): any {
       return {};
     },
-    getTimeParams(): any { 
+    getTimeParams (): any {
       const { DEFAULT_ZONE } = generateZoneOptions();
 
       return {
         zone: searchParams?.zone || DEFAULT_ZONE,
         period: searchParams?.period || PERIOD_OPTIONS[0],
       };
-    }
+    },
   });
 
   const usp = new URLSearchParams(searchParams);
+  const imageUsp = new URLSearchParams(usp);
+  imageUsp.set('image_size', 'auto');
 
   return (
     <ShareBlock
       title={title ?? 'Untitled'}
       url={`${siteConfig.host}/widgets/${params.vendor}/${params.name}?${usp.toString()}`}
-      thumbnailUrl={`${siteConfig.host}/widgets/${params.vendor}/${params.name}/thumbnail.png?${usp.toString()}`}
+      thumbnailUrl={`${siteConfig.host}/widgets/${params.vendor}/${params.name}/thumbnail.png?${imageUsp.toString()}`}
       keywords={keywords}
     />
   );
