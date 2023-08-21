3e994268ea82d7c85595d60520900802599b1010
Jagger <634750802@qq.com>
2023-08-21 17:18:50 +0800

    chore: update widget home data

--- packages/ui/src/components/CodeBlock/CopyButton.tsx
+++ packages/ui/src/components/CodeBlock/CopyButton.tsx
@@ -17,8 +17,9 @@ export function CopyButton ({ className, content }: { className: string, content
   }, [content]);
 
   return (
-    <button className={clsx('border bg-toolbar rounded opacity-40 hover:opacity-100 transition-opacity p-0.5 text-active', className)} onClick={handleCopy} type="button">
-      {checked ? <ClipboardCheckIcon width={12} height={12} /> : <ClipboardIcon width={12} height={12} />}
+    <button className={clsx('border bg-toolbar rounded opacity-60 hover:opacity-100 transition-opacity px-2 py-1 text-active flex gap-2 items-center text-sm', className)} onClick={handleCopy} type="button">
+      {checked ? <ClipboardCheckIcon width={14} height={14} /> : <ClipboardIcon width={14} height={14} />}
+      {checked ? 'Copied!' : 'Copy'}
     </button>
   );
 }
--- web/app/widgets/outlinks.tsx
+++ web/app/widgets/outlinks.tsx
@@ -3,7 +3,7 @@ import GitHubIcon from 'bootstrap-icons/icons/github.svg'
 
 const links = [
   {title: 'Twitter post', href: '', icon: <TwitterIcon width={14} height={14} />},
-  {title: 'GitHub README', href: '', icon: <GitHubIcon width={14} height={14} />},
+  {title: 'GitHub README', href: 'https://github.com/pingcap/ossinsight#repo-insight-widget', icon: <GitHubIcon width={14} height={14} />},
 ];
 
 export default links;
