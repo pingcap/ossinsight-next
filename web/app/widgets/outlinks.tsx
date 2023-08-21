24678b1fb56e5096a476f3899d3be7b31e1f7299
Hooopo <Hoooopo@gmail.com>
2023-08-21 20:19:37 +0800

    Update outlinks.tsx

--- web/app/widgets/outlinks.tsx
+++ web/app/widgets/outlinks.tsx
@@ -2,8 +2,8 @@ import TwitterIcon from 'bootstrap-icons/icons/twitter.svg'
 import GitHubIcon from 'bootstrap-icons/icons/github.svg'
 
 const links = [
-  {title: 'Twitter post', href: '', icon: <TwitterIcon width={14} height={14} />},
-  {title: 'GitHub README', href: 'https://github.com/pingcap/ossinsight#repo-insight-widget', icon: <GitHubIcon width={14} height={14} />},
+  {title: 'Twitter post', href: 'https://twitter.com/OSSInsight/status/1693597860468777107', icon: <TwitterIcon width={14} height={14} />},
+  {title: 'GitHub README', href: 'https://github.com/pingcap/ossinsight#feature-0-shareable-insight-widgets', icon: <GitHubIcon width={14} height={14} />},
 ];
 
 export default links;
