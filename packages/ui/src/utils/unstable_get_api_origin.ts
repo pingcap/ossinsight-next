5498306a5980bf264017e58d68371c80d0d8a07e
Jagger <634750802@qq.com>
2023-08-30 14:36:53 +0800

    fix

--- packages/ui/src/utils/unstable_get_api_origin.ts
+++ packages/ui/src/utils/unstable_get_api_origin.ts
@@ -1,5 +1,5 @@
 export function unstable_getApiOrigin () {
-  if (typeof location !== 'undefined') {
+  if (typeof location !== 'undefined' && location.port !== '6006' /* storybook */) {
     return location.origin + '/unstable_proxy';
   } else {
     return 'https://api.ossinsight.io';
