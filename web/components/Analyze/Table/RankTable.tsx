de9a5b331ba3e5ec4d1e017897d28cabc32c5e04
czhen <56986964+shczhen@users.noreply.github.com>
2023-09-04 13:34:32 +0800

    refine rank table

--- web/components/Analyze/Table/RankTable.tsx
+++ web/components/Analyze/Table/RankTable.tsx
@@ -1,50 +1,116 @@
 'use client';
 import * as React from 'react';
 import { SingleToggleButtonGroup } from '@ossinsight/ui/src/components/Toggle/ToggleButton';
+import { Tabs, Tab } from '@ossinsight/ui/src/components/Tabs';
+
+const people = [
+  {
+    name: 'Lindsay Walton',
+    title: 'Front-end Developer',
+    email: 'lindsay.walton@example.com',
+    role: 'Member',
+  },
+  {
+    name: 'Lindsay Walton',
+    title: 'Front-end Developer',
+    email: 'lindsay.walton@example.com',
+    role: 'Member',
+  },
+  {
+    name: 'Lindsay Walton',
+    title: 'Front-end Developer',
+    email: 'lindsay.walton@example.com',
+    role: 'Member',
+  },
+  {
+    name: 'Lindsay Walton',
+    title: 'Front-end Developer',
+    email: 'lindsay.walton@example.com',
+    role: 'Member',
+  },
+  {
+    name: 'Lindsay Walton',
+    title: 'Front-end Developer',
+    email: 'lindsay.walton@example.com',
+    role: 'Member',
+  },
+  // More people...
+];
+
+const data = new Array(10)
+  .fill(0)
+  .map((_, i) => [i + 1, `Company-${i}`, i + 95]);
 
 export default function RankTable(props: any) {
   const [sortBy, setSortBy] = React.useState<'new' | 'total'>('new');
 
   return (
-    <div className='flex flex-col items-center justify-between'>
-      <h2 className='text-2xl font-bold'>Top Companies</h2>
-      <SingleToggleButtonGroup
-        value={sortBy}
-        onValueChange={(value) => setSortBy(value as any)}
-        items={[
-          {
-            id: 'new',
-            label: sortBy === 'new' ? '⬇️ New' : 'New',
-            value: 'new',
-            className: 'px-4',
-            disabled: sortBy === 'new',
-          },
-          {
-            id: 'total',
-            label: sortBy === 'total' ? '⬇️ Total' : 'Total',
-            value: 'total',
-            className: 'px-4',
-            disabled: sortBy === 'total',
-          },
-        ]}
-        className=''
-      />
-      <table className='table-auto'>
-        <thead>
-          {/* <tr>
-            <th>Top Companies</th>
-          </tr> */}
-        </thead>
-        <tbody>
-          {new Array(10).fill(0).map((_, i) => (
-            <tr key={i}>
-              <td>{i + 1}</td>
-              <td className='px-4'>Company-{i}</td>
-              <td className='text-end'>{i + 95}⬆️</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <>
+      <div className='px-4 sm:px-6 lg:px-8 flex flex-col'>
+        <h1 className='px-1 text-base font-semibold leading-6 text-white'>
+          Top locations
+        </h1>
+        <Tabs className=''>
+          <Tab title='New' value='new'>
+            <Table header={['No.', 'Company1', 'Activities']} data={data} />
+          </Tab>
+          <Tab title='Total' value='total'>
+            <Table header={['No.', 'Company2', 'Activities']} data={data} />
+          </Tab>
+        </Tabs>
+      </div>
+    </>
+  );
+}
+
+function Table(props: {
+  data: Array<any>;
+  header: Array<string | React.ReactElement>;
+}) {
+  const { data, header } = props;
+
+  return (
+    <div className='mt-1 flow-root'>
+      <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+        <div className='inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8'>
+          <table className='min-w-full divide-y divide-gray-700'>
+            <thead>
+              <tr>
+                <th
+                  scope='col'
+                  className='py-1.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0'
+                >
+                  {header[0]}
+                </th>
+                {header.slice(1).map((h) => (
+                  <th
+                    key={h.toString()}
+                    scope='col'
+                    className='px-3 py-1.5 text-left text-sm font-semibold text-white'
+                  >
+                    {h}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className='divide-y divide-gray-800'>
+              {data.map((row) => (
+                <tr key={row[0]}>
+                  <td className='whitespace-nowrap py-1 pl-4 pr-3 text-sm font-medium text-white sm:pl-0'>
+                    {row[0]}
+                  </td>
+                  <td className='whitespace-nowrap px-3 py-1 text-sm text-gray-300'>
+                    {row[1]}
+                  </td>
+                  <td className='whitespace-nowrap px-3 py-1 text-sm text-gray-300'>
+                    {row[2]}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
