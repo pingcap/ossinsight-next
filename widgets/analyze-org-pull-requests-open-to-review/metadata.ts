320a56813be0c4e82e64cbe05720372daceca164
czhen <56986964+shczhen@users.noreply.github.com>
2023-09-11 22:53:03 +0900

    add widget

---
+++ widgets/analyze-org-pull-requests-open-to-review/metadata.ts
@@ -0,0 +1,16 @@
+import {
+  MetadataGenerator,
+  WidgetVisualizerContext,
+} from '@ossinsight/widgets-types';
+
+const generateMetadata: MetadataGenerator<{ owner_id: string }> = ({
+  parameters: { owner_id },
+  getRepo,
+}) => {
+  // const main = getRepo(parseInt(repo_id));
+  return {
+    title: `TODO Open to review of ${owner_id}`,
+  };
+};
+
+export default generateMetadata;
