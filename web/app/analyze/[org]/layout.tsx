ea4b79b9544d7258d6708f0be685805af175ef10
czhen <56986964+shczhen@users.noreply.github.com>
2023-09-12 14:07:48 +0900

    refine layout

--- web/app/analyze/[org]/layout.tsx
+++ web/app/analyze/[org]/layout.tsx
@@ -2,11 +2,11 @@ import OrgNav from '@/components/Analyze/Navigation/OrgNav';
 
 export default function UserOrgAnalyzeLayout({
   children,
-  params
+  params,
 }: {
-    children: React.ReactNode;
-    params: { org: string };
-  }) {
+  children: React.ReactNode;
+  params: { org: string };
+}) {
   const { org } = params;
 
   return (
@@ -14,11 +14,12 @@ export default function UserOrgAnalyzeLayout({
       <div className='flex'>
         <div className='flex w-full flex-col md:flex-row'>
           <aside className='bg-toolbar border-r md:min-h-[calc(100vh-var(--site-header-height))]'>
-            {/* <div className='p-4 sticky h-full overflow-y-auto max-h-[calc(100vh-var(--site-header-height))] styled-scrollbar'> */}
-            <OrgNav org={org} />
+            <div className='sticky top-[var(--site-header-height)] h-full overflow-y-auto max-h-[calc(100vh-var(--site-header-height))] styled-scrollbar'>
+              <OrgNav org={org} />
+            </div>
           </aside>
           <main className='flex-1 overflow-x-hidden'>
-            <div className='p-2'>{children}</div>
+            <div className='pl-6 pr-24 py-8'>{children}</div>
           </main>
         </div>
       </div>
