29fced6e413c3822796d28f073d7a04a557c8e59
Jagger <634750802@qq.com>
2023-09-22 12:04:47 +0800

    chore: update deps

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -17,10 +17,6 @@ const nextConfig = {
   experimental: {
     externalDir: true,
     serverComponentsExternalPackages: ['@napi-rs/canvas'],
-    outputFileTracingIgnores: [
-      'node_modules/.pnpm/@swc+core-linux-x64-musl@1.3.70',
-      'node_modules/.pnpm/@swc+core-linux-x64-gnu@1.3.70',
-    ]
   },
   webpack: config => {
     config.externals.push('@napi-rs/canvas')
