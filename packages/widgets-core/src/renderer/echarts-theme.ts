ef89d1978dd629ed60c4ce40d6349bbfcd773eee
Jagger <634750802@qq.com>
2023-09-22 11:27:05 +0800

    revert colors

--- packages/widgets-core/src/renderer/echarts-theme.ts
+++ packages/widgets-core/src/renderer/echarts-theme.ts
@@ -1,7 +1,7 @@
 import { registerTheme } from 'echarts';
 
 export const darkColorPalette = ['#dd6b66', '#759aa0', '#e69d87', '#8dc1a9', '#ea7e53', '#eedd78', '#73a373', '#73b9bc', '#7289ab', '#91ca8c', '#f49f42'];
-export const lightColorPalette = ['#facfcb', '#d5e8de', '#e69d87', '#8dc1a9', '#ea7e53', '#eedd78', '#73a373', '#73b9bc', '#7289ab', '#91ca8c', '#f49f42'];
+export const lightColorPalette = ['#dd6b66', '#759aa0', '#e69d87', '#8dc1a9', '#ea7e53', '#eedd78', '#73a373', '#73b9bc', '#7289ab', '#91ca8c', '#f49f42'];
 
 function registerThemeDark () {
   const contrastColor = '#979797';
