78ea1ce14af254a4b04879cde32d1db2cf25a966
Jagger <634750802@qq.com>
2023-09-26 16:54:16 +0800

    split codes

---
+++ web/components/Scrollspy/hooks.ts
@@ -0,0 +1,16 @@
+import { useScrollspyContext } from '@/components/Scrollspy/ScrollspyContext';
+import { useContext, useEffect, useState } from 'react';
+
+export function useScrollspyCurrentSection () {
+  const spy = useScrollspyContext();
+  const [current, setCurrent] = useState(spy.currentSection);
+
+  useEffect(() => {
+    spy.subscribe(setCurrent);
+    return () => {
+      spy.unsubscribe(setCurrent);
+    };
+  }, [spy]);
+
+  return current;
+}
