61c1705e5cff654b9a2563064b375cee97f4be95
Jagger <634750802@qq.com>
2023-10-11 16:44:53 +0800

    seo: add seo info for analyze org page

--- web/app/analyze/[owner]/page.tsx
+++ web/app/analyze/[owner]/page.tsx
@@ -2,7 +2,8 @@ import Content from '@/app/analyze/[owner]/content';
 import { fetchOwnerInfo } from '@/app/analyze/[owner]/fetchOwner';
 import OrgAnalyzePageHeader from '@/components/Analyze/Header/OrgHeader';
 import AnalyzeOwnerContextProvider from '@/components/Context/Analyze/AnalyzeOwner';
-import { notFound, redirect } from 'next/navigation';
+import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 
 export default async function Page ({ params }: { params: { owner: string } }) {
   const data = await fetchOwnerInfo(decodeURIComponent(params.owner));
@@ -17,3 +18,37 @@ export default async function Page ({ params }: { params: { owner: string } }) {
     redirect(`https://ossinsight.io/analyze/${params.owner}`);
   }
 }
+
+export async function generateMetadata ({ params }: { params: { owner: string } }): Promise<Metadata> {
+  const data = await fetchOwnerInfo(decodeURIComponent(params.owner));
+  if (data.type === 'Organization') {
+    const title = `Analyze ${data.name} | OSSInsight`
+    const description = 'Unlock the power of time-flexible organization analytics, community recognition metrics, participant insights, and productivity analysis with OSSInsight.';
+    return {
+      title,
+      description,
+      keywords: [
+        'OSSInsight',
+        'open-source company',
+        'organization analytics',
+        'GitHub organization',
+        'star growth',
+        'collaboration metrics',
+        'participant insights',
+        'productivity analysis',
+      ],
+      twitter: {
+        title,
+        description,
+      },
+      openGraph: {
+        title,
+        description,
+      }
+    };
+  } else {
+    return {};
+  }
+}
+
+
