845e59f2f35caaabbdc89f0842b5f3420f46037b
czhen <56986964+shczhen@users.noreply.github.com>
2023-10-17 17:14:52 +0800

    Update BackButton.tsx

--- web/app/widgets/[vendor]/[name]/BackButton.tsx
+++ web/app/widgets/[vendor]/[name]/BackButton.tsx
@@ -1,11 +1,16 @@
 'use client';
+import * as React from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function BackButton () {
   const router = useRouter();
 
+  const handleClickCallback = React.useCallback(() => {
+    router.push('/widgets');
+  }, [router]);
+
   return (
-    <button onClick={router.back} className="Button Button-secondary inline-flex items-center gap-1 mb-4">
+    <button onClick={handleClickCallback} className="Button Button-secondary inline-flex items-center gap-1 mb-4">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
