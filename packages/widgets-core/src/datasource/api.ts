0ec0e6380f2fd6c507038162f152d79d93bf28eb
czhen <56986964+shczhen@users.noreply.github.com>
2023-10-26 15:53:00 +0800

    fix: add protocol scheme

--- packages/widgets-core/src/datasource/api.ts
+++ packages/widgets-core/src/datasource/api.ts
@@ -21,7 +21,7 @@ export default async function executeApiDatasource (config: ApiDatasourceConfig,
   // TODO: replaceAll is a workaround, e.g. /api/queries/orgs/issues%2Fclosed-ratio => /api/queries/orgs/issues/closed-ratio
   const urlExpanded = template.expand(ctx.parameters).replaceAll(`%2F`, `/`);
   let url: string | URL = '';
-  if (ctx.runtime === 'server') { 
+  if (ctx.runtime === 'server') {
     url = new URL(urlExpanded, getBaseUrl());
     setUrlParams(url.searchParams, config.params ?? {}, ctx.parameters);
   } else {
@@ -29,7 +29,7 @@ export default async function executeApiDatasource (config: ApiDatasourceConfig,
     setUrlParams(urlSearchParams, config.params ?? {}, ctx.parameters);
     url = `${url}?${urlSearchParams.toString()}`;
   }
-  
+
   const response = await fetch(url, { signal });
 
   if (!response.ok) {
@@ -75,18 +75,20 @@ function getBaseUrl() {
     process.env.NEXT_PUBLIC_VERCEL_ENV ||
     process.env.VERCEL_ENV ||
     process.env.NODE_ENV;
-  const url =
-    process.env.NEXT_PUBLIC_VERCEL_URL ||
-    process.env.VERCEL_URL ||
-    `http://localhost:${process.env.PORT || 3000}`;
   switch (env) {
     case 'production':
       // TODO: change to https://ossinsight.io when we have promoted to production
       return `https://next.ossinsight.io`;
     case 'preview':
-      return process.env.NEXT_PUBLIC_VERCEL_URL || process.env.VERCEL_URL;
+      const previewHost =
+        process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL || process.env.VERCEL_BRANCH_URL;
+      return `https://${previewHost}`;
     case 'development':
     default:
-      return url;
+      const devHost =
+        process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL || process.env.VERCEL_BRANCH_URL;
+      return devHost
+        ? `https://${devHost}`
+        : `http://localhost:${process.env.PORT || 3000}`;
   }
 }
