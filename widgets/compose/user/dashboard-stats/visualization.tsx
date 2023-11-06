4167a655d69c568f0144c30cccb2f4ba83de86f7
czhen <56986964+shczhen@users.noreply.github.com>
2023-11-06 10:48:29 +0800

    refine langs

--- widgets/compose/user/dashboard-stats/visualization.tsx
+++ widgets/compose/user/dashboard-stats/visualization.tsx
@@ -121,6 +121,57 @@ const mockLanguages = [
   },
 ];
 
+const mock12Langs = [
+  {
+    name: 'JavaScript',
+    percent: 0.3,
+  },
+  {
+    name: 'TypeScript',
+    percent: 0.2,
+  },
+  {
+    name: 'Python',
+    percent: 0.2,
+  },
+  {
+    name: 'Go',
+    percent: 0.1,
+  },
+  {
+    name: 'C++',
+    percent: 0.1,
+  },
+  {
+    name: 'Java',
+    percent: 0.05,
+  },
+  {
+    name: 'Rust',
+    percent: 0.05,
+  },
+  {
+    name: 'Ruby',
+    percent: 0.05,
+  },
+  {
+    name: 'PHP',
+    percent: 0.05,
+  },
+  {
+    name: 'C#',
+    percent: 0.05,
+  },
+  {
+    name: 'C',
+    percent: 0.05,
+  },
+  {
+    name: 'Shell',
+    percent: 0.05,
+  },
+];
+
 const languangeColors = [
   '#f1e05a',
   '#2b7489',
@@ -145,10 +196,22 @@ const languangeColors = [
   '#c22d40',
 ];
 
+const handleLangs = (langs: typeof mockLanguages, maxLangs = 7) => { 
+  const sortedLangs = langs.sort((a, b) => b.percent - a.percent);
+  if (sortedLangs.length <= maxLangs) return sortedLangs;
+  const top7Langs = sortedLangs.slice(0, maxLangs);
+  const otherLangs = sortedLangs.slice(maxLangs);
+  const otherLangsPercent = otherLangs.reduce((acc, cur) => acc + cur.percent, 0);
+  return [...top7Langs, { name: 'Others', percent: otherLangsPercent }];
+}
+
 export default function (
   input: Input,
   ctx: WidgetVisualizerContext<Params>
 ): Compose.JSX.Element {
+
+  const handledLangs = handleLangs(mock12Langs);
+
   return (
     <Card title={`vitalovevitamin's GitHub Dashboard`} subtitle=' '>
       <grid rows={1} cols={2} gap={4} data={mockItems} ifEmpty='indicator'>
@@ -205,21 +268,21 @@ export default function (
           <flex direction='vertical' grow={0.3}>
             {/* <builtin-label label={'bar'} /> */}
             <builtin-progress-bar
-              items={mockLanguages.map((lang, idx) => ({
+              items={handledLangs.map((lang, idx) => ({
                 label: lang.name,
                 percentage: lang.percent,
                 color: languangeColors[idx],
               }))}
             />
             <flex direction='vertical' gap={2}>
-              {mockLanguages
+              {handledLangs
                 .reduce((acc, cur, idx) => {
                   if (idx % 4 === 0) {
                     acc.push([]);
                   }
                   acc[acc.length - 1].push(cur);
                   return acc;
-                }, [] as (typeof mockLanguages)[])
+                }, [] as (typeof handledLangs)[])
                 .map((langs, idx1) => (
                   <flex direction='horizontal' gap={2}>
                     {langs.map((lang, idx2) => (
