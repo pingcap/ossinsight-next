1ae968376c56b8992ccf4f631e5f3b2496401895
Mini256 <minianter@foxmail.com>
2023-11-07 16:21:11 +0800

    add without cache endpoint

---
+++ web/app/api/queries/without-cache/route.ts
@@ -0,0 +1,40 @@
+import {APIError, DataService} from "@/app/api/queries/[...query]/data-service";
+import {endpoints} from "@/app/api/queries/[...query]/endpoints";
+import { NextRequest, NextResponse } from 'next/server';
+
+export const runtime = 'edge';
+
+// Configure to us-west-2 region, make edge functions close to the database.
+export const preferredRegion = 'pdx1';
+
+const dataService = new DataService({
+  url: process.env.DATABASE_URL
+});
+
+export async function GET(req: NextRequest) {
+  // Remove prefix.
+  const queryName = req.nextUrl.pathname.replaceAll('/api/queries/', '');
+  const endpoint = endpoints[queryName];
+  if (!endpoint) {
+    return new Response(JSON.stringify({ message: 'Endpoint not found.' }), {
+      status: 404,
+    });
+  }
+
+  let result;
+  try {
+    result = await dataService.handleQueryEndpoint(req, endpoint.templateSQL, endpoint.config);
+  } catch (err: any) {
+    if (err instanceof APIError) {
+      return new Response(JSON.stringify({ message: err.message }), {
+        status: err.statusCode,
+      });
+    } else {
+      return new Response(JSON.stringify({ message: "Internal Server Error" }), {
+        status: 500,
+      });
+    }
+  }
+
+  return NextResponse.json(result);
+}
