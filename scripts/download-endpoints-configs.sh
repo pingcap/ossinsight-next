035d86c705e510a3141a06a41682f4d37e2e77e4
Jagger <634750802@qq.com>
2023-11-10 13:21:27 +0800

    fix

--- scripts/download-endpoints-configs.sh
+++ scripts/download-endpoints-configs.sh
@@ -2,6 +2,11 @@
 
 ENDPOINT_BASEDIR="./endpoints/"
 
+if test -d ${ENDPOINT_BASEDIR} && test ! $CI; then
+  echo endpoints exists
+  exit 0
+fi;
+
 # Download endpoints config from pingcap/ossinsight repo.
 rm -rf ./ossinsight-main.zip
 curl -LJO https://github.com/pingcap/ossinsight/archive/refs/heads/main.zip;
